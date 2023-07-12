28cc9ec2fa32132f476e0509c261e38565eeb100
F1uke Jessada <26509248+lzy3me@users.noreply.github.com>
2023-07-12 22:34:48 +0700

    added logo

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -2,7 +2,7 @@ import "./index.css"
 
 function Logo() {
   return (
-    <section className="flex p-5 lg:pl-[20rem] min-h-screen flex-1 flex-col justify-center">
+    <section className="flex min-h-screen flex-1 flex-col justify-center">
       <h1 className="relative py-2 logo-blur-1st">
         F1uke.JS
         <span className="pr-5"></span>
